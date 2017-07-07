8d696090e2b17fe802034af4e006d74444dede4c
Wallison Freitas <wallisonfreitas00@gmail.com>
2017-07-07 00:18:59 -0300

    Apagando Linhas Criadas

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,7 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente){                                           //forEach = parecido com o laço for, só que tem como parametro de limite o tamanho do meu array, que neste caso é o array pacientes;
-  paciente.addEventListener("dblclick", function(){                             //dblclick = é um evento de duplo click
-    console.log(paciente);
-    this.remove();                                                              //remove() = Função de Remoção / this = palavra de contexto que Significa esta; faz com que o atributo que recebeu o duplo click seja escutado; este evento está atrelado ao paciente;
-  });
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+  event.target.parentNode.remove();                                             //parentNode(nó pai) = referente ao nó acima do target que foi clicado.
 });
