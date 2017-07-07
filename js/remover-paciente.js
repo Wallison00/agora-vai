2aeda138dd9191deaeccd770a5dc3dc3be59e7e8
Wallison Freitas <wallisonfreitas00@gmail.com>
2017-07-07 00:55:15 -0300

    animação no delete

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,9 @@
-var pacientes = document.querySelectorAll(".paciente");
-
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event){
-  event.target.parentNode.remove();                                             //parentNode(nó pai) = referente ao nó acima do target que foi clicado.
+  event.target.parentNode.classList.add("fadeOut");
+
+  setTimeout(function(){                                                        // setTimeout = Cria uma espera para que a função dentro dela só seja executada depois de toda a função anterior a ela já terminada;
+    event.target.parentNode.remove();
+  }, 1000);                                                                     //1000 = referente a 1000 milissegundos o que dá 1.0 segundo;
 });
